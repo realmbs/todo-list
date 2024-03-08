b0b184912c7b67023e3c4248448a108ef581af26
realmbs <119774290+realmbs@users.noreply.github.com>
2024-03-08 16:36:40 -0600

    vite.config.js updated to incl /list/index.html

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         form: resolve(__dirname, 'form/index.html'),
+        list: resolve(__dirname, 'list/index.html'),
       },
     },
   },
