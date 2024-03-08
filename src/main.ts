714cf21fd8224df46b2f66d1e634e701bb76f244
realmbs <119774290+realmbs@users.noreply.github.com>
2024-03-08 15:53:34 -0600

    configuring multi page app > vite.config.js

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,3 @@
 import './style.css'
-import todoItem from './todoItem.ts'
+import todoItem from '../form/todoItem.ts'
 
-const app = document.getElementById('app') as HTMLDivElement;
-const test = new todoItem('test', false, new Date(), 1, 'test');
-
-const todoCard = document.createElement('div');
-todoCard.classList.add('todo__card');
-
-const todoTitle = document.createElement('h3');
-todoTitle.textContent = test.title;
-todoCard.appendChild(todoTitle);
-
-app.appendChild(todoCard);
-
-console.log(test.getDetails());
