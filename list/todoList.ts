ede364a7263e68d95ab1fd259e9bd54ca7b58443
realmbs <119774290+realmbs@users.noreply.github.com>
2024-03-08 16:55:07 -0600

    todoItem rendering to dom, UPNEXT: add multiple items, delete items

--- list/todoList.ts
+++ list/todoList.ts
@@ -6,3 +6,34 @@ console.log(todoList);
 
 const app = document.getElementById('app') as HTMLDivElement;
 
+todoList.forEach(todo => {
+  const todoDiv = document.createElement('div') as HTMLDivElement;
+  todoDiv.classList.add('todo');
+
+  const title = document.createElement('h3') as HTMLHeadingElement;
+  title.innerText = todo.title;
+  todoDiv.appendChild(title);
+
+  const completed = document.createElement('p') as HTMLParagraphElement;
+  completed.innerText = `Completed: ${todo.completed}`;
+  todoDiv.appendChild(completed);
+
+  const dueDate = document.createElement('p') as HTMLParagraphElement;
+  dueDate.innerText = `Due Date: ${todo.dueDate}`;
+
+  if (todo.dueDate) {
+    todoDiv.appendChild(dueDate);
+  } else {
+    dueDate.style.display = 'none';
+  }
+
+  const priority = document.createElement('p') as HTMLParagraphElement;
+  priority.innerText = `Priority: ${todo.priority}`;
+  todoDiv.appendChild(priority);
+
+  const notes = document.createElement('p') as HTMLParagraphElement;
+  notes.innerText = `Notes: ${todo.notes}`;
+  todoDiv.appendChild(notes);
+
+  app.appendChild(todoDiv);
+})
